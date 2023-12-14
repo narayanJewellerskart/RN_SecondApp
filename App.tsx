5a5a7a5ec2dc61779be3ca6384595f19c3cdf487
narayanJewellerskart <narayan@jewellerskart.com>
2023-12-14 18:25:14 +0530

    second commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {View, Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <ScrollView>
+      <ScrollView style={styles.sectionBg}>
         <FlatCards />
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  sectionBg: {
+    backgroundColor: '#27272a',
+  },
+});
 
 export default App;
--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -4,9 +4,57 @@ import React from 'react';
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text style={styles.cardText}>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text style={styles.cardText}>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text style={styles.cardText}>Purple</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+    color: 'white',
+    letterSpacing: 2,
+    textTransform: 'uppercase',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: 3,
+  },
+  cardText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    letterSpacing: 2,
+    fontSize: 14,
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#15803d',
+  },
+  cardThree: {
+    backgroundColor: '#6d28d9',
+  },
+});
